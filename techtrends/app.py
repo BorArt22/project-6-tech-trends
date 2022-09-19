6201328036eccbd44dd38cefd44a2720e6ddb332
BorArt22 <borart22@gmail.com>
2022-09-19 13:52:26 +0200

    add logging and new pages - status, metrics

--- techtrends/app.py
+++ techtrends/app.py
@@ -3,11 +3,19 @@ import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+from datetime import datetime
+import logging
+
+# Count all database connections
+connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -22,12 +30,62 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
+#Function to log messages
+def log_message(message):
+    app.logger.info('{time}, {message}'.format(
+        time=datetime.now().strftime("%d/%m/%Y, %H:%M:%S"), message=message))
+
+# Healthcheck endpoint
+@app.route('/status')
+def healthcheck():
+    try:
+        connection = get_db_connection()
+        connection.execute('SELECT * FROM posts').fetchall()
+        connection.close()
+        response = app.response_class(
+                response=json.dumps({"result":"OK - healthy"}),
+                status=200,
+                mimetype='application/json'
+        )
+
+        log_message('Status request successfull')
+        return response
+
+    except Exception:
+        response = app.response_class(
+            response=json.dumps({"result":"ERROR - unhealthy"}),
+            status=500,
+            mimetype='application/json'
+        )
+        
+        log_message('Status request unsuccessfull - ERROR')
+        return response
+
+# Metrics endpoint
+@app.route('/metrics')
+def metrics():
+
+    connection = get_db_connection()
+    posts_count = connection.execute('SELECT count(1) as posts_count FROM posts').fetchall()
+    connection.close()
+
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"db_connection_count":connection_count,"post_count":posts_count['posts_count']}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    log_message('[Metrics] request')
+    return response
+
+
 # Define the main route of the web application 
 @app.route('/')
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    log_message('The main route of the web application request')
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
@@ -36,13 +94,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        log_message('Article "{post_id}" can not be find'.format(post_id = post_id))
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        log_message('Article "{post_id}" retrieved'.format(post_id = post_id))
+        return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    log_message('[about] request')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -53,6 +114,7 @@ def create():
         content = request.form['content']
 
         if not title:
+            log_message('[create] return error because Title is empty')
             flash('Title is required!')
         else:
             connection = get_db_connection()
@@ -61,10 +123,15 @@ def create():
             connection.commit()
             connection.close()
 
+            log_message('[create] Article "{title}" created'.format(title = title))
+
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    ## stream logs to app.log file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
+
+    app.run(host='0.0.0.0', port='3111')
